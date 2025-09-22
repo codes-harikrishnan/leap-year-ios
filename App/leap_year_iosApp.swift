232f68f02ddf4d8f8369f084c97faf57317a5e33
Harikrishnan <118582559+2022-DEV3-086@users.noreply.github.com>
2025-09-22 11:56:29 +0200

    Initial project setup

---
+++ App/leap_year_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  leap_year_iosApp.swift
+//  leap-year-ios
+//
+//  Created by Harikrishnan S on 22/09/25.
+//
+
+import SwiftUI
+
+@main
+struct leap_year_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
